2781661ce0f6e4061279789521e4956f20ac64c2
GauravSonawane <gauravsonawane1368xd@gmail.com>
2022-01-04 00:49:48 +0530

    .b2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 export default function App() {
   return (
-    <div>
-      <div className="bg-primary text-light p-4">Hello worlddd</div>
+    <div className="comtainer-fluid">
+      <div className="row bg-dark text-light p-3">
+        <div className="col h3">React</div>
+      </div>
+      <div className="row">
+        <div className="col-12 bg-secondary text-light p-3">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+          similique deserunt quia magni. Amet suscipit vitae alias delectus
+          accusantium mollitia, nam sapiente quas? Optio consequuntur adipisci
+          aliquam. Maxime, inventore voluptas?
+        </div>
+      </div>
     </div>
   );
 }

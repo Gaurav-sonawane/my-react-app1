6336488cdf67679ea104c1f30197a2fff6519260
GauravSonawane <gauravsonawane1368xd@gmail.com>
2022-01-04 01:30:15 +0530

    .b4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 export default function App() {
-  let list = [1, 1, 1, 1, 1, 1, 1, 1];
   return (
-    <div className="container-fluid">
-      <div className="row bg-dark text-light p-3">
-        <div className="col h3">React</div>
-      </div>
+    <div>
+      <Hellow />
+    </div>
+  );
+}
 
-      {list.map((item) => (
-        <div className="row">
-          <div className="col-12 bg-secondary text-light p-3 mb-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            consequuntur odio totam nulla qui velit ex laborum cupiditate
-            ratione vel? Cumque optio nemo fugiat veritatis dolorum! Deleniti
-            asperiores totam animi?
-          </div>
-        </div>
-      ))}
+function Hellow() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero quod
+        dolores, cupiditate nisi facere accusantium aut ipsa nobis eveniet. Nam
+        molestiae sunt ipsa dolore debitis et quo iusto ducimus nobis!
+      </p>
     </div>
   );
 }

670d83ae3f705a02294f241552a2c7f677b23c35
GauravSonawane <gauravsonawane1368xd@gmail.com>
2022-01-04 23:45:31 +0530

    .b9

--- src/App.js
+++ src/App.js
@@ -1,40 +1,91 @@
 import { useState } from "react";
+import { NavItem } from "react-bootstrap";
 
 export default function App() {
   return (
     <div>
-      <MyComponent />
+      <MyFun />
     </div>
   );
 }
+function MyFun() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Mobile, setMobile] = useState("");
 
-function MyComponent() {
-  const [list, setList] = useState(["Hello world...."]);
+  const [userList, setUserList] = useState([]);
 
-  const tweetHere = () => {
-    const newList = [...list, "hello universe"];
-
-    setList(newList);
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMobile = (e) => {
+    setMobile(e.target.value);
   };
 
-  const deleteTweet = () => {
-    list.splice(0, 1);
+  const registerUser = () => {
+    const user = {
+      username: username,
+      password: password,
+      email: Email,
+      Mobile: Mobile,
+    };
 
-    const newList = [...list];
-    setList(newList);
+    const newList = [user, ...userList];
+    setUserList(newList);
   };
 
   return (
     <div>
-      <h1>Working with input element</h1>
-      <input type="button" value="tweet to everyone" onClick={tweetHere} />
-      <input type="button" value="delete first tweet" onClick={deleteTweet} />
-
+      <h1>Working With Registration</h1>
       <div>
-        {list.map((item) => (
-          <div>{item}</div>
-        ))}
+        <input
+          type="text"
+          value={username}
+          onChange={handleUsername}
+          placeholder="enter username"
+        />
       </div>
+      <div>
+        <input
+          type="text"
+          value={password}
+          onChange={handlePassword}
+          placeholder="enter password"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={Email}
+          onChange={handleEmail}
+          placeholder="enter Email"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={Mobile}
+          onChange={handleMobile}
+          placeholder="enter Mobile"
+        />
+      </div>
+      <div>
+        <input type="button" value="Register" onClick={registerUser} />
+      </div>
+      <hr />
+
+      {userList.map((item) => (
+        <div>
+          {item.username},{item.password},{item.email},{item.Mobile}
+        </div>
+      ))}
     </div>
   );
 }
